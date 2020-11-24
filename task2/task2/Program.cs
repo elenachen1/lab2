fd4cd5c7266e61d63714d62963172027609a0852
Lena <elenachen624>
2020-11-24 23:45:55 +0300

    task 2

---
+++ task2/task2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double first, second;
+            string action;
+
+            Console.WriteLine("Введите первое число:");
+            first = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            second = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Выберите операцию: '+','-','*','/'");
+            action = Console.ReadLine();
+
+            switch (action)
+            {
+                case "+":
+                    Console.WriteLine(first + second);
+                    break;
+                case "-":
+                    Console.WriteLine(first - second);
+                    break;
+
+                case "*":
+                    Console.WriteLine(first * second);
+                    break;
+
+                case "/":
+                    if (second == 0)
+                    {
+                        Console.WriteLine("Внимание! Деение на ноль запрещено!");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine(first / second);
+                    }
+
+                    break;
+
+                default:
+                    Console.WriteLine("Неизвестна операция");
+                    break;
+
+            }
+
+        }
+    }
+}
